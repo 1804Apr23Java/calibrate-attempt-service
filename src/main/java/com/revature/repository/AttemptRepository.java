ec3ee8b4610f8110f1eb92059546386e0ceaabce
anqiwang2018 <38628176+anqiwang2018@users.noreply.github.com>
2018-06-20 15:51:14 -0400

    removed duplicate

--- src/main/java/com/revature/repository/AttemptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.beans.Attempt;
-
-public interface AttemptRepository extends JpaRepository<Attempt, Integer> {
-	public Attempt findAttemptbyUser(String email);
-	public Attempt findAttemptbyId(int id);
-}
